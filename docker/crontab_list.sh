5e76178b4fad12c8f90b160a4fc1954c0c778d59
flatpig <jzclc@126.com>
2021-10-23 19:37:12 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -191,7 +191,7 @@
 # 双11环游记
 5 0,6,14 * * * node /scripts/jd_hyj.js >> /scripts/logs/jd_hyj.log 2>&1
 # 选品官
-23 9,10 * * * node /scripts/jd_selectionOfficer.js >> /scripts/logs/jd_selectionOfficer.log 2>&1
+23 9,19 * * * node /scripts/jd_selectionOfficer.js >> /scripts/logs/jd_selectionOfficer.log 2>&1
 # 跳跳乐瓜分京豆
 1 0,11,21 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 # 手机狂欢城
