5f83c62fe2790ea676eeb90eabb234a2f4aa7ded
grays.chen <grays.chen@dfgroup.pro>
2021-10-26 10:26:26 +0800

    delete 发财大赢家

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -210,11 +210,3 @@
 20 11,16 1-2,25-31 10,11 * node /scripts/jd_zzt.js >> /scripts/logs/jd_zzt.log 2>&1
 # 省钱大赢家之翻翻乐
 20 0,6-23 * * * node /scripts/jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
-#发财大赢家
-1 1,18 * * * node /scripts/jd_fc.js >> /scripts/logs/jd_fc.log 2>&1
-#发财大赢家助力
-1 1,18 * * * node /scripts/jd_fcdyj_help.js >> /scripts/logs/jd_fcdyj_help.log 2>&1
-#发财大赢家微信助力-快速
-17 0,5 * * * node /scripts/jd_dyj_help_wx.js >> /scripts/logs/jd_dyj_help_wx.log 2>&1
-#发财大赢家助力-快速
-11 0,5 * * * node /scripts/jd_dyj_help.js >> /scripts/logs/jd_dyj_help.log 2>&1
