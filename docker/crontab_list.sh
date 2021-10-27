3f179e8b51e32df0b8b4f09b19afd2646424117f
grays.chen <grays.chen@dfgroup.pro>
2021-10-27 17:32:58 +0800

    delete jd_cash

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -97,7 +97,7 @@
 # 京喜农场
 30 9,12,18 * * * node /scripts/jd_jxnc.js >> /scripts/logs/jd_jxnc.log 2>&1
 # 签到领现金
-10 */4 * * * node /scripts/jd_cash.js >> /scripts/logs/jd_cash.log 2>&1
+#10 */4 * * * node /scripts/jd_cash.js >> /scripts/logs/jd_cash.log 2>&1
 # 闪购盲盒
 47 8,,16,22 * * * node /scripts/jd_sgmh.js >> /scripts/logs/jd_sgmh.log 2>&1
 # 京东秒秒币
