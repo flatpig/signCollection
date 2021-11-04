94f7b8b3b04d89884f919ed92cee8e02934a0739
grays.chen <grays.chen@dfgroup.pro>
2021-11-04 10:45:05 +0800

    delete car_change

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -91,7 +91,7 @@
 # 微信小程序京东赚赚
 6 0-5/1,11 * * * node /scripts/jd_jdzz.js >> /scripts/logs/jd_jdzz.log 2>&1
 # 京东汽车旅程赛点兑换金豆
-59 23 * * * node /scripts/jd_car_exchange.js >> /scripts/logs/jd_car_exchange.log 2>&1
+# 59 23 * * * node /scripts/jd_car_exchange.js >> /scripts/logs/jd_car_exchange.log 2>&1
 # 导到所有互助码
 23 7 * * * node /scripts/jd_get_share_code.js >> /scripts/logs/jd_get_share_code.log 2>&1
 # 京喜农场
