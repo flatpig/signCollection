5f9a48f2adae5001a0ed956121f9b5efe61133e5
grays.chen <grays.chen@dfgroup.pro>
2021-11-18 13:55:29 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -133,7 +133,8 @@
 59 7,15,23 * * * node /scripts/jd_joy_reward_Mod.js >> /scripts/logs/jd_joy_reward_Mod.log 2>&1
 # 京喜购物返红包助力
 4 0-23/6 * * * node /scripts/jd_jx_cashback.js >> /scripts/logs/jd_jx_cashback.log 2>&1
-
+# 跳跳乐
+1 0,11,21 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 # 京东图形签到
 14 2,10 * * * node /scripts/jd_sign_graphics.js >> /scripts/logs/jd_sign_graphics.log 2>&1
 # 京喜大作战
