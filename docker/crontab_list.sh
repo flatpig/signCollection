ccda61c92b3e275762a954d7e2627cfca0744bc2
grays.chen <grays.chen@dfgroup.pro>
2021-11-24 10:28:41 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -157,4 +157,4 @@
 # 财富岛合成生鲜兑换
 0 12 * * * node /scripts/jd_cfd_fresh_exchange.js >> /scripts/logs/jd_cfd_fresh_exchange.log 2>&1
 # 京东答题
-22 6,18 * 11,12 * node /scripts/jd_jddt.js >> /scripts/logs/jd_jddt.log 2>&1
+22 6,18 * * * node /scripts/jd_jddt.js >> /scripts/logs/jd_jddt.log 2>&1
