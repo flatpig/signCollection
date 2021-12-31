95817dcaec48bfbe7b0f916e3d63d5ba43434455
grays.chen <grays.chen@dfgroup.pro>
2021-12-31 13:25:28 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -148,7 +148,8 @@
 # 跳跳乐
 #1 0,11,21 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 # 京东图形签到
-14 2,10 * * * node /scripts/jd_sign_graphics.js >> /scripts/logs/jd_sign_graphics.log 2>&1
+3 0 * * * node /scripts/jd_m_sign.js >> /scripts/logs/jd_m_sign.log 2>&1
+#14 2,10 * * * node /scripts/jd_sign_graphics.js >> /scripts/logs/jd_sign_graphics.log 2>&1
 # 京喜大作战
 39 1,14 * * * node /scripts/jd_jxdzz.js >> /scripts/logs/jd_jxdzz.log 2>&1
 # 签到免单
